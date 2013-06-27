41f09949e73bdd73a0d367999585d9b768808e61
dacaiguoguo <dacaiguoguo@163.com>
2013-06-27 20:50:03 +0800

    fix master
    add sdwebimage 3.3

--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -51,19 +51,49 @@
 
 // SDWebImage
 #define COCOAPODS_POD_AVAILABLE_SDWebImage
-#define COCOAPODS_VERSION_MAJOR_SDWebImage 2
-#define COCOAPODS_VERSION_MINOR_SDWebImage 7
-#define COCOAPODS_VERSION_PATCH_SDWebImage 4
+#define COCOAPODS_VERSION_MAJOR_SDWebImage 3
+#define COCOAPODS_VERSION_MINOR_SDWebImage 3
+#define COCOAPODS_VERSION_PATCH_SDWebImage 0
 
-// SDWebImage/Main
-#define COCOAPODS_POD_AVAILABLE_SDWebImage_Main
-#define COCOAPODS_VERSION_MAJOR_SDWebImage_Main 2
-#define COCOAPODS_VERSION_MINOR_SDWebImage_Main 7
-#define COCOAPODS_VERSION_PATCH_SDWebImage_Main 4
+// SDWebImage/MapKit
+#define COCOAPODS_POD_AVAILABLE_SDWebImage_MapKit
+#define COCOAPODS_VERSION_MAJOR_SDWebImage_MapKit 3
+#define COCOAPODS_VERSION_MINOR_SDWebImage_MapKit 3
+#define COCOAPODS_VERSION_PATCH_SDWebImage_MapKit 0
 
 // cdebug
 #define COCOAPODS_POD_AVAILABLE_cdebug
 #define COCOAPODS_VERSION_MAJOR_cdebug 0
 #define COCOAPODS_VERSION_MINOR_cdebug 1
 #define COCOAPODS_VERSION_PATCH_cdebug 1
+
+// libwebp
+#define COCOAPODS_POD_AVAILABLE_libwebp
+#define COCOAPODS_VERSION_MAJOR_libwebp 0
+#define COCOAPODS_VERSION_MINOR_libwebp 3
+#define COCOAPODS_VERSION_PATCH_libwebp 0
+
+// libwebp/dec
+#define COCOAPODS_POD_AVAILABLE_libwebp_dec
+#define COCOAPODS_VERSION_MAJOR_libwebp_dec 0
+#define COCOAPODS_VERSION_MINOR_libwebp_dec 3
+#define COCOAPODS_VERSION_PATCH_libwebp_dec 0
+
+// libwebp/dsp
+#define COCOAPODS_POD_AVAILABLE_libwebp_dsp
+#define COCOAPODS_VERSION_MAJOR_libwebp_dsp 0
+#define COCOAPODS_VERSION_MINOR_libwebp_dsp 3
+#define COCOAPODS_VERSION_PATCH_libwebp_dsp 0
+
+// libwebp/utils
+#define COCOAPODS_POD_AVAILABLE_libwebp_utils
+#define COCOAPODS_VERSION_MAJOR_libwebp_utils 0
+#define COCOAPODS_VERSION_MINOR_libwebp_utils 3
+#define COCOAPODS_VERSION_PATCH_libwebp_utils 0
+
+// libwebp/webp
+#define COCOAPODS_POD_AVAILABLE_libwebp_webp
+#define COCOAPODS_VERSION_MAJOR_libwebp_webp 0
+#define COCOAPODS_VERSION_MINOR_libwebp_webp 3
+#define COCOAPODS_VERSION_PATCH_libwebp_webp 0
 
