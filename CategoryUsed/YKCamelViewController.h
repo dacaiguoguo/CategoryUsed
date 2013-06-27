108e8d5a7df98743d8d3635876544427c49c3b4e
dacaiguoguo <dacaiguoguo@163.com>
2013-06-27 14:19:35 +0800

    Initial Commit

---
+++ CategoryUsed/YKCamelViewController.h
@@ -0,0 +1,13 @@
+//
+//  YKCamelViewController.h
+//  CategoryUsed
+//
+//  Created by yanguo.sun on 13-6-27.
+//  Copyright (c) 2013年 YEK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YKCamelViewController : UIViewController
+
+@end
---
+++ CategoryUsed/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CategoryUsed
+//
+//  Created by yanguo.sun on 13-6-27.
+//  Copyright (c) 2013年 YEK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YKCamelAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YKCamelAppDelegate class]));
+    }
+}
