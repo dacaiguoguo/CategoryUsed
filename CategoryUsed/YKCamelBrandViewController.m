dc24ca9d64ef7e3e15d12ca15af90133176ca66e
dacaiguoguo <dacaiguoguo@163.com>
2013-07-02 11:36:16 +0800

    test extkeypathcoding.h

--- CategoryUsed/YKCamelBrandViewController.m
+++ CategoryUsed/YKCamelBrandViewController.m
@@ -9,31 +9,73 @@
 #import "YKCamelBrandViewController.h"
 #import "YKLoopScrollView.h"
 #import "UIImageView+WebCache.h"
+#import "EXTKeyPathCoding.h"
+
 @interface YKCamelBrandViewController ()<YKLoopScrollViewDelegate>
 @property (nonatomic, retain) IBOutlet YKLoopScrollView *loopScrollView ;
 @property (strong, nonatomic) IBOutlet UIImageView *loopBackImageView;
 @property (strong, nonatomic)  NSMutableDictionary *loopCacheDic;
-@property (strong, nonatomic)  NSMutableArray *loopCacheArray;
 
 @property (nonatomic, retain) IBOutlet UIPageControl *pageControl;
 @end
 
 @implementation YKCamelBrandViewController
 
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        self.title = @"品牌馆";
+        self.tabBarItem.image = [UIImage imageNamed:[self navTabNormalImageNames][1]];
+        self.loopCacheDic = [NSMutableDictionary new];
+        // Custom initialization
+    }
+    return self;
+}
 
 
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    BOOL isPhone5 = [[UIScreen mainScreen] bounds].size.height==568;
+    if (isPhone5) {
+        self.loopBackImageView.frame = CGRectMake(15, 60, 290, 260);
+        self.loopScrollView.frame = self.loopBackImageView.frame;
+    }else{
+        self.loopBackImageView.frame = CGRectMake(15, 15, 290, 260);
+        self.loopScrollView.frame = self.loopBackImageView.frame;
+    }
+    
+    [self requestBrandInfo];
+
+    
+    _pageControl.currentPage = 0;
+    // Do any additional setup after loading the view from its nib.
+}
+- (void)viewDidUnload {
+    [self setLoopBackImageView:nil];
+    [super viewDidUnload];
+}
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context{
+    NSLog(@"%@\n%@",keyPath,object);
+    NSLog(@"%@",((YKHome *)object).htmlUrl);
+    
+    
+}
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    [[SDImageCache sharedImageCache] clearMemory];
+    // Dispose of any resources that can be recreated.
+}
 /*
  总共有多少页
  */
 -(int) numOfPageForScrollView:(YKLoopScrollView*) ascrollView{
-    if (self.brandList!=nil) {
-        return self.brandList.count;
-
-    }else{
-        _pageControl.numberOfPages = self.loopCacheArray.count;
-        _pageControl.currentPage = 0;
-       return self.loopCacheArray.count;
-    }
+    int ret = 0;
+    ret = self.brandList.count;
+    return ret;
 }
 
 /*
@@ -44,7 +86,6 @@
     
     UIView *ret = [self.loopCacheDic objectForKey:[NSString stringWithFormat:@"image%d",apageIndex]];
     if (ret) {
-        NSLog(@"%p",ret);
         return ret;
     }
     NSURL *url;
@@ -57,9 +98,6 @@
     }
 
     assert(url);
-
-    
-
     
     [self.loopCacheDic setObject:image forKey:[NSString stringWithFormat:@"image%d",apageIndex]];
     return image;
@@ -74,17 +112,6 @@
 
 
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        self.title = @"品牌馆";
-        self.tabBarItem.image = [UIImage imageNamed:[self navTabNormalImageNames][1]];
-        self.loopCacheDic = [NSMutableDictionary new];
-        // Custom initialization
-    }
-    return self;
-}
 
 - (NSString *)imagesFilePath{
     NSArray *psths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
@@ -108,44 +135,12 @@
     }];
 }
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-
-    BOOL isPhone5 = [[UIScreen mainScreen] bounds].size.height==568;
-    if (isPhone5) {
-        self.loopBackImageView.frame = CGRectMake(15, 60, 290, 260);
-        self.loopScrollView.frame = self.loopBackImageView.frame;
-    }else{
-        self.loopBackImageView.frame = CGRectMake(15, 15, 290, 260);
-        self.loopScrollView.frame = self.loopBackImageView.frame;
-    }
-    [self requestBrandInfo];
-    [self.loopScrollView reloadData];
-    _pageControl.currentPage = 0;
-    [self.view addSubview:_pageControl];
-    // Do any additional setup after loading the view from its nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    [[SDImageCache sharedImageCache] clearMemory];
-    // Dispose of any resources that can be recreated.
-}
-//@property (strong) NSString* brandId;
-//@property (strong) NSString* title;
-//@property (strong) NSString* content;
-//@property (strong) NSString* imageUrl;
 
 - (void)goBrandCategoryForBI:(NSString *)_braid{
 
 }
 - (IBAction)goBrandCategory:(id)sender {
 #ifdef DEBUG
-    [self.brandList enumerateObjectsWithOptions:NSEnumerationReverse usingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
-        *stop = NO;
-    }];
     [self.brandList enumerateObjectsUsingBlock:^(YKBrand* obj, NSUInteger idx, BOOL *stop) {
         NSLog(@"brandId:%@,title:%@,content:%@,imageUrl:%@",obj.brandId,obj.title,obj.content,obj.imageUrl);
     }];
@@ -154,15 +149,11 @@
     NSString *title = ((YKBrand*)[self.brandList objectAtIndex:self.pageControl.currentPage]).title;
     [self goBrandCategoryForBI:brandid];
     if (brandid.length==0) {
-
         return;
     }
     brandid  = brandid.length>0?brandid:@"";
     title = title.length>0 ?title:@"品牌分类";
 //    [[[YKModuleManager shareInstance] shareNavModule] gotoControllerWithName:YK_MODULE_NAME_BRAND_CATEGORY params:@{@"brandId":brandid,@"title":title} fromController:self sender:sender];
 }
-- (void)viewDidUnload {
-    [self setLoopBackImageView:nil];
-    [super viewDidUnload];
-}
+
 @end
